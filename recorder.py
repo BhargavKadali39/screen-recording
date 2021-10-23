ccc997c00cbb0fbd5e9f48de68bfe3a73df3ede1
Bhargav <bhargavkadali39@gmail.com>
2021-10-24 00:02:36 +0530

    Update recorder.py

--- recorder.py
+++ recorder.py
@@ -1,1 +1,22 @@
 
+import cv2
+import numpy as np
+import pyautogui
+def recordery():
+    SCREEN_SIZE = (1920, 1080)
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    out = cv2.VideoWriter("output.avi", fourcc, 20.0, (SCREEN_SIZE))
+    while True:
+        img = pyautogui.screenshot()
+        frame = np.array(img)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        out.write(frame)
+        # if the user clicks q, it exitsq
+        if cv2.waitKey(1) & 0xFF == "q":
+            return 'done'
+    cv2.destroyAllWindows()
+    out.release()
+
+chs = input("Enter any value to start recording")
+chs = recordery()
+print(chs)
